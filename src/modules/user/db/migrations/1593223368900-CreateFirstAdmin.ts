e14094cec1e663ec196e202d562d4f75a8dc949a
Evgenii Goriachkin <Evgenii.Goriachkin@payever.org>
2021-06-10 19:18:57 +0300

    disabled init migration

--- src/modules/user/db/migrations/1593223368900-CreateFirstAdmin.ts
+++ src/modules/user/db/migrations/1593223368900-CreateFirstAdmin.ts
@@ -5,29 +5,29 @@ import * as bcrypt from 'bcrypt';
 export class CreateFirstAdmin1593223368900 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        const userRepository: Repository<User> = queryRunner.connection.getRepository(User);
+        // const userRepository: Repository<User> = queryRunner.connection.getRepository(User);
 
-        if (await userRepository.findOne({where: {login: 'admin'}})) {
-            return;
-        }
+        // if (await userRepository.findOne({where: {login: 'admin'}})) {
+        //     return;
+        // }
 
-        const user: User = userRepository.create({
-            login: 'admin',
-            passwordHash: await bcrypt.hash('secret', 10),
-            name: 'GromMax'
-        });
+        // const user: User = userRepository.create({
+        //     login: 'admin',
+        //     passwordHash: await bcrypt.hash('secret', 10),
+        //     name: 'GromMax'
+        // });
 
-        await userRepository.insert(user);
+        // await userRepository.insert(user);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        const userRepository: Repository<User> = queryRunner.connection.getRepository(User);
-        const user: User = await userRepository.findOne({where: {login: 'admin'}});
-        if (!user) {
-            return;
-        }
+        // const userRepository: Repository<User> = queryRunner.connection.getRepository(User);
+        // const user: User = await userRepository.findOne({where: {login: 'admin'}});
+        // if (!user) {
+        //     return;
+        // }
 
-        await userRepository.remove(user);
+        // await userRepository.remove(user);
     }
 
 }
